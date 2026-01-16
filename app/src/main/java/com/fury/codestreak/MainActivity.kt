273cb15f57d6b9a49e4f3904f6e61913afa2d488
Arjun Tyagi <tyagiarjun1809@gmail.com>
2026-01-16 13:09:55 +0530

    build: Connect Firebase, set up MainActivity

--- app/src/main/java/com/fury/codestreak/MainActivity.kt
+++ app/src/main/java/com/fury/codestreak/MainActivity.kt
@@ -7,41 +7,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.fury.codestreak.presentation.theme.BackgroundDark
 import com.fury.codestreak.presentation.theme.CodeStreakTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CodeStreakTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = BackgroundDark
+                ) {
+                    // We will add our Navigation here in the next step
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodeStreakTheme {
-        Greeting("Android")
     }
 }
