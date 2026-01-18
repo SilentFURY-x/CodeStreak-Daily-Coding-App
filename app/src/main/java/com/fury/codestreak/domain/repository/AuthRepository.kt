8ba2787ff97454f00abf408f654e2f89d197a7ed
Arjun Tyagi <tyagiarjun1809@gmail.com>
2026-01-18 16:31:58 +0530

    refactor: Move AuthRepo to designated folder

--- app/src/main/java/com/fury/codestreak/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/fury/codestreak/domain/repository/AuthRepository.kt
@@ -1,13 +1,14 @@
 package com.fury.codestreak.domain.repository
 
-import com.google.firebase.auth.FirebaseUser
+import android.app.Activity
 import com.fury.codestreak.util.Resource
+import com.google.firebase.auth.FirebaseUser
 
 interface AuthRepository {
     suspend fun login(email: String, password: String): Resource<FirebaseUser>
     suspend fun signup(email: String, password: String): Resource<FirebaseUser>
     // New methods
     suspend fun signInWithGoogle(idToken: String): Resource<FirebaseUser>
-    suspend fun signInWithGithub(activity: android.app.Activity): Resource<FirebaseUser>
+    suspend fun signInWithGithub(activity: Activity): Resource<FirebaseUser>
     fun getCurrentUser(): FirebaseUser?
 }
